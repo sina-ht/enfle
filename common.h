1b2ecff8f99df8cb4973ca251ea0dc8c4d5639aa
Hiroshi Takekawa <sian@fennel.org>
2000-10-10 11:46:53 +0000

    Coloring debug message.

--- common.h
+++ common.h
@@ -3,8 +3,8 @@
  * (C)Copyright 2000 by Hiroshi Takekawa
  * This file is part of Enfle.
  *
- * Last Modified: Tue Oct 10 05:04:22 2000.
- * $Id: common.h,v 1.2 2000/10/09 20:29:56 sian Exp $
+ * Last Modified: Tue Oct 10 16:39:20 2000.
+ * $Id: common.h,v 1.3 2000/10/10 11:46:53 sian Exp $
  *
  * Enfle is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
@@ -32,14 +32,16 @@
 
 #ifdef DEBUG
 #  define PROGNAME PACKAGE "-debug"
-#  define debug_message(format, args...) fprintf(stderr, format, ## args)
+#  define debug_message(format, args...) fprintf(stderr, "\033[01m\033[31m" format "\033[00m", ## args)
 #  define IDENTIFY_BEFORE_OPEN
 #  define IDENTIFY_BEFORE_LOAD
+#  define IDENTIFY_BEFORE_PLAY
 #else
 #  define PROGNAME PACKAGE
 #  define debug_message(format, args...)
-/* #  undef IDENTIFY_BEFORE_OPEN */
-/* #  undef IDENTIFY_BEFORE_LOAD */
+/* #  define IDENTIFY_BEFORE_OPEN */
+/* #  define IDENTIFY_BEFORE_LOAD */
+/* #  define IDENTIFY_BEFORE_PLAY */
 #endif
 
 #ifdef WITH_DMALLOC
