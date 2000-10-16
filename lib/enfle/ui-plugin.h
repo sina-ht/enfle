b915ee2dea7d511a362a6c9484cc19227a1637ba
Hiroshi Takekawa <sian@fennel.org>
2000-10-16 19:30:29 +0000

    Added bits_per_pixel.

--- lib/enfle/ui-plugin.h
+++ lib/enfle/ui-plugin.h
@@ -3,8 +3,8 @@
  * (C)Copyright 2000 by Hiroshi Takekawa
  * This file is part of Enfle.
  *
- * Last Modified: Thu Oct 12 19:09:30 2000.
- * $Id: ui-plugin.h,v 1.3 2000/10/12 15:47:02 sian Exp $
+ * Last Modified: Sun Oct 15 22:20:35 2000.
+ * $Id: ui-plugin.h,v 1.4 2000/10/16 19:30:29 sian Exp $
  *
  * Enfle is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
@@ -35,7 +35,7 @@ typedef struct _ui_data UIData;
 
 struct _ui_screen {
   unsigned int width, height;
-  int depth;
+  int depth, bits_per_pixel;
   void *private;
 };
 
