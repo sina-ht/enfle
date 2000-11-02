d748b196ada3b8e9684f088e59e174e80c22315b
Hiroshi Takekawa <sian@fennel.org>
2000-11-02 19:36:19 +0000

    Inlined mem_free().

--- lib/dllloader/mm.h
+++ lib/dllloader/mm.h
@@ -10,21 +10,24 @@
 #include <stdlib.h>
 
 #ifdef W32API_REQUEST_MEM_ALLOC
-static inline void *w32api_mem_alloc(int size)
+static inline
+void *w32api_mem_alloc(int size)
 {
   return (void *)malloc(size);
 }
 #endif
 
 #ifdef W32API_REQUEST_MEM_REALLOC
-static inline void *w32api_mem_realloc(void *p, int size)
+static inline
+void *w32api_mem_realloc(void *p, int size)
 {
   return (void *)realloc(p, size);
 }
 #endif
 
 #ifdef W32API_REQUEST_MEM_FREE
-static void w32api_mem_free(void *p)
+static inline
+void w32api_mem_free(void *p)
 {
   if (p)
     free(p);
