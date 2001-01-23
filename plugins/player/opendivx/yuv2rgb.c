2a915fb661b9f068756454e972adcf145e1d87ac
Hiroshi Takekawa <sian@fennel.org>
2001-01-23 19:19:31 +0000

    Cleanup.

--- plugins/player/opendivx/yuv2rgb.c
+++ plugins/player/opendivx/yuv2rgb.c
@@ -13,6 +13,14 @@
  *
  * YUV to RGB Conversion
  *
+ * R = 1.164*(Y - 16) + 1.596*(V-128)
+ * G = 1.164*(Y - 16) - 0.813*(U-128) - 0.391*(V-128)
+ * B = 1.164*(Y - 16) + 2.018*(U-128)
+ *
+ * R = (74 * (Y - 16) + 102 * (V - 128)) >> 6
+ * G = (74 * (Y - 16) -  52 * (U - 128) - 25 * (V - 128)) >> 6
+ * B = (74 * (Y - 16) + 129 * (U - 128)) >> 6
+ *
  * R = ((Y << 14) + 22970 * (V - 128)) >> 14
  * G = ((Y << 14) -  5638 * (U - 128) - 11697 * (V - 128)) >> 14
  * B = ((Y << 14) + 29029 * (U - 128)) >> 14
@@ -31,6 +39,8 @@
 #include <stdio.h>
 #include "divxdecore/yuv2rgb.h"
 
+#define clip(v) if (v < 0) v = 0; else if (v > 255) v = 255;
+
 /* YUV12 -> ARGB32 */
 void
 yuv2rgb_32(uint8_t *y, int stride_y,
@@ -38,40 +48,20 @@ yuv2rgb_32(uint8_t *y, int stride_y,
 	   uint8_t *out, int width, int height)
 {
   int i, j;
-  uint8_t *yl, *ul, *vl;
   int r, g, b;
+  uint8_t *yl, *ul, *vl;
+  uint32_t *outp = (uint32_t *)out;
 
   yl = y;
   ul = u;
   vl = v;
   for (i = 0; i < height; i++) {
     for (j = 0; j < width; j++) {
-#if 0
-      out++;
-      *out++ = ((yl[j] << 14) + 22970 * (vl[j >> 1] - 128)) >> 14;
-      *out++ = ((yl[j] << 14) +  5638 * (ul[j >> 1] - 128) - 11697 * (vl[j >> 1] - 128)) >> 14;
-      *out++ = ((yl[j] << 14) + 29029 * (ul[j >> 1] - 128)) >> 14;
-#else
       r = ((yl[j] << 14) + 22970 * (vl[j >> 1] - 128)) >> 14;
       g = ((yl[j] << 14) +  5638 * (ul[j >> 1] - 128) - 11697 * (vl[j >> 1] - 128)) >> 14;
       b = ((yl[j] << 14) + 29029 * (ul[j >> 1] - 128)) >> 14;
-      if (r < 0)
-	r = 0;
-      else if (r > 255)
-	r = 255;
-      if (g < 0)
-	g = 0;
-      else if (g > 255)
-	g = 255;
-      if (b < 0)
-	b = 0;
-      else if (b > 255)
-	b = 255;
-      out[1] = r;
-      out[2] = g;
-      out[3] = b;
-      out += 4;
-#endif
+      clip(r); clip(g); clip(b);
+      *outp++ = (r << 16) | (g << 8) | b;
     }
     yl += stride_y;
     if (i % 2) {
@@ -88,15 +78,19 @@ yuv2rgb_24(uint8_t *y, int stride_y,
 {
   int i, j;
   uint8_t *yl, *ul, *vl;
+  int r, g, b;
 
   yl = y;
   ul = u;
   vl = v;
   for (i = 0; i < height; i++) {
     for (j = 0; j < width; j++) {
-      *out++ = ((yl[j] << 14) + 22970 * (vl[j >> 1] - 128)) >> 14;
-      *out++ = ((yl[j] << 14) +  5638 * (ul[j >> 1] - 128) - 11697 * (vl[j >> 1] - 128)) >> 14;
-      *out++ = ((yl[j] << 14) + 29029 * (ul[j >> 1] - 128)) >> 14;
+      r = ((yl[j] << 14) + 22970 * (vl[j >> 1] - 128)) >> 14;
+      g = ((yl[j] << 14) +  5638 * (ul[j >> 1] - 128) - 11697 * (vl[j >> 1] - 128)) >> 14;
+      b = ((yl[j] << 14) + 29029 * (ul[j >> 1] - 128)) >> 14;
+      clip(r); clip(g); clip(b);
+      out[0] = r; out[1] = g; out[2] = b;
+      out += 3;
     }
     yl += stride_y;
     if (i % 2) {
@@ -112,7 +106,8 @@ yuv2rgb_16(uint8_t *y, int stride_y,
 	   uint8_t *out, int width, int height)
 {
   int i, j;
-  uint8_t *yl, *ul, *vl, r, g, b;
+  int r, g, b;
+  uint8_t *yl, *ul, *vl;
   uint16_t *outp = (uint16_t *)out;
 
   yl = y;
@@ -123,6 +118,7 @@ yuv2rgb_16(uint8_t *y, int stride_y,
       r = ((yl[j] << 14) + 22970 * (vl[j >> 1] - 128)) >> 14;
       g = ((yl[j] << 14) +  5638 * (ul[j >> 1] - 128) - 11697 * (vl[j >> 1] - 128)) >> 14;
       b = ((yl[j] << 14) + 29029 * (ul[j >> 1] - 128)) >> 14;
+      clip(r); clip(g); clip(b);
       *outp++ = ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3);
     }
     yl += stride_y;
