c900faf7125c8158d9bb8d4494548510d55a5d2c
Hiroshi Takekawa <sian@fennel.org>
2002-08-17 02:21:49 +0000

    New file.

---
+++ plugins/player/avifile/config.h
@@ -0,0 +1,21 @@
+/*
+ * config.h -- dummy config file
+ * (C)Copyright 2002 by Hiroshi Takekawa
+ * This file is part of Enfle.
+ *
+ * Last Modified: Sat Jul 20 20:52:17 2002.
+ * $Id: config.h,v 1.1 2002/08/17 02:21:49 sian Exp $
+ *
+ * Enfle is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Enfle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
