e3fdcf5f7d32dba313be04e5b4dcd5632bdc673c
Hiroshi Takekawa <sian@fennel.org>
2004-04-05 15:44:54 +0000

    New file.

---
+++ lib/enfle/demultiplexer_bitmapinfoheader.h
@@ -0,0 +1,41 @@
+/*
+ * demultiplexer_bitmapinfoheader.h
+ * (C)Copyright 2001-2004 by Hiroshi Takekawa
+ * This file is part of Enfle.
+ *
+ * Last Modified: Tue Apr  6 00:11:12 2004.
+ * $Id: demultiplexer_bitmapinfoheader.h,v 1.1 2004/04/05 15:44:54 sian Exp $
+ *
+ * Enfle is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Enfle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#if !defined(__DEMULTIPLEXER_BITMAPINFOHEADER_H__)
+#define __DEMULTIPLEXER_BITMAPINFOHEADER_H__
+
+typedef struct __bitmapinfoheader BITMAPINFOHEADER;
+struct __attribute__((__packed__)) __bitmapinfoheader {
+  DWORD biSize;         /* size of this structure */
+  LONG  biWidth;        /* width of image */
+  LONG  biHeight;       /* width of height */
+  WORD  biPlanes;       /* must be 1 */
+  WORD  biBitCount;     /* bits per pixel */
+  DWORD biCompression;  /* compression type */
+  DWORD biSizeImage;    /* size of image */
+  LONG  biXPixPerMeter; /* horizontal resolution */
+  LONG  biYPixPerMeter; /* vertical resolution */
+  DWORD biClrUsed;      /* color table related */
+  DWORD biClrImportant; /* ditto */
+};
+
+#endif
