607b10e82e3f649c25ec0a4f406235b11dfd12cf
Hiroshi Takekawa <sian@fennel.org>
2004-04-05 15:42:05 +0000

    Clear m->bitrate, m->block_align.

--- lib/enfle/identify.c
+++ lib/enfle/identify.c
@@ -3,8 +3,8 @@
  * (C)Copyright 2001, 2002 by Hiroshi Takekawa
  * This file is part of Enfle.
  *
- * Last Modified: Wed Mar 31 23:15:45 2004.
- * $Id: identify.c,v 1.14 2004/03/31 14:33:33 sian Exp $
+ * Last Modified: Mon Apr  5 23:35:53 2004.
+ * $Id: identify.c,v 1.15 2004/04/05 15:42:05 sian Exp $
  *
  * Enfle is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
@@ -119,7 +119,7 @@ identify_stream(EnflePlugins *eps, Image *p, Movie *m, Stream *s, VideoWindow *v
   /* for generic player */
   if (m && demultiplexer_identify(eps, m, s, c) == DEMULTIPLEX_OK) {
     debug_message_fnc("demultiplexer_identify() OK.\n");
-    m->channels = m->samplerate = 0;
+    m->channels = m->samplerate = m->bitrate = m->block_align = 0;
     if ((m->demux = demultiplexer_examine(eps, m->format, m, s, c)) == NULL) 
       return IDENTIFY_STREAM_MOVIE_FAILED;
     debug_message_fnc("demultiplexer_examine() OK.\n");
